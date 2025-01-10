b4a68dbe19e057fef5d9190b1780f22df4993a74
Advait Deshmukh <36404945+advait-d@users.noreply.github.com>
2025-01-10 12:09:32 +0530

    add vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "./ThemeProvider";
+import { Analytics } from "@vercel/analytics/react"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,6 +20,7 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning>
       <body className={`${inter.className} dark:bg-medium`}>
         <ThemeProvider>{children}</ThemeProvider>
+        <Analytics/>
       </body>
     </html>
   );
